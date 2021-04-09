85cd26a55937361ed280ea646f585eb30bae8a49
Gabriel Frael <gfrael@gmail.com>
2021-04-09 11:14:34 -0300

    Primeiro commit.

---
+++ src/main/java/com/nelioalves/cursomc/Principal.java
@@ -0,0 +1,14 @@
+package com.nelioalves.cursomc;
+
+import java.util.List;
+
+import com.amazonaws.services.macie.model.ListS3ResourcesRequest;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+//		List.
+		
+	}
+}
